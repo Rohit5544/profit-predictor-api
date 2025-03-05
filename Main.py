e349dfb25bc1b2a77e08099c62b6a4931e640ae1
Rohit5544 <rohitsharma@gmail.com>
2025-03-05 13:50:49 -0600

    Add files via upload
    Initial commit

---
+++ Main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import numpy as np
+import pickle
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Load your saved model and encoder
+model = pickle.load(open("model.pkl", "rb"))
+encoder = pickle.load(open("encoder.pkl", "rb"))
+
+# Define input structure
+class ProfitPredictionInput(BaseModel):
+    rd_spend: float
+    admin_spend: float
+    marketing_spend: float
+    state: str
+
+@app.get("/")
+def home():
+    return {"message": "Profit Predictor API by Zara"}
+
+@app.post("/predict")
+def predict_profit(input: ProfitPredictionInput):
+    state_encoded = encoder.transform([[input.state]])
+    finput = np.concatenate((state_encoded, [[input.rdSpend, input.admSpend, input.marketingSpend]]), axis=1)
+    profit = model.predict(finput)
+    return {"predicted_profit": float(profit[0])}
